3c52b846df09f0538045d9988877adf318630af8
Peter <deadforstree@yandex.ru>
2022-12-15 15:48:59 +0300

    warning

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,35 @@
 const aboutButton = document.querySelector('.profile__edit-btn');
 const aboutPopup = document.querySelector('.popup');
 const aboutButtonClose = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__form');
-let UserName = document.querySelector('.profile__title');
-let UserText = document.querySelector('.profile__subtitle');
-let NameInput = document.querySelector('.popup__item_type_name');
-let textInput = document.querySelector('.popup__item_type_about');
-const popupSubmit = document.querySelector('popup__save-btn');
+const formElement = document.querySelector('.popup__form');
+const userName = document.querySelector('.profile__title');
+const userText = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.popup__item_type_name');
+const textInput = document.querySelector('.popup__item_type_about');
+
+function PopupClose() {
+    aboutPopup.classList.remove('popup__opened');
+};
 
 aboutButton.addEventListener('click', (evt) => {
     evt.preventDefault();
-    aboutPopup.classList.add('popup_opened');
-    NameInput.value = UserName.textContent;
-    textInput.value = UserText.textContent;
+    aboutPopup.classList.add('popup__opened');
+    nameInput.value = userName.textContent;
+    textInput.value = userText.textContent;
 });
 
 aboutButtonClose.addEventListener('click', () => {
-    aboutPopup.classList.remove('popup_opened')
+    PopupClose();
 });
 
-function handleFormSubmit(evt) {
-    evt.preventDefault();
-    UserName.textContent = NameInput.value;
-    UserText.textContent = textInput.value;
-    aboutPopup.classList.remove('popup_opened')
+function handleFormSubmit(evtm) {
+    evtm.preventDefault();
+    userName.textContent = nameInput.value;
+    userText.textContent = textInput.value;
+    PopupClose();
 };
 
 formElement.addEventListener('submit', handleFormSubmit);
+aboutButtonClose.addEventListener('click', () => {
+    aboutPopup.classList.remove('popup__opened')
+});
