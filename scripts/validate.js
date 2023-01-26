33165a7b087543982a70bf7656400f1e093f1ae8
Peter <deadforstree@yandex.ru>
2023-01-26 15:11:37 +0300

    valid

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__item',
+  submitButtonSelector: '.popup__save-btn',
+  inactiveButtonClass: 'popup__save-btn_disabled',
+  inputErrorClass: 'popup__item_type_error',
+  errorClass: 'popup__error_visible'
+});
